e6845c5588ef9bc28612eba71ab0f3699153f104
Nathan <dynamonath@nathanjwtx.me>
2018-08-02 12:06:25 -0500

    Completed Player and Enemy code.

--- Characters/Character.cs
+++ Characters/Character.cs
@@ -1,23 +1,68 @@
 using Godot;
 using System;
 
+
 public class Character : Area2D
 {
-    // Member variables here, example:
-    // private int a = 2;
-    // private string b = "textvar";
+    [Export] public int Speed;
+
+    private int _tileSize = 64;
+    protected bool CanMove = true;
+    protected string Facing = "right";
+    private Tween _moveTween;
+
+    public Dictionary<string, Vector2> _moves = new Dictionary<string, Vector2>
+    {
+        {"right", new Vector2(1, 0)},
+        {"left", new Vector2(-1, 0)},
+        {"up", new Vector2(0, -1)},
+        {"down", new Vector2(0, 1)}
+    };
+
+    public Dictionary<string, RayCast2D> Raycasts;
 
     public override void _Ready()
     {
-        // Called every time the node is added to the scene.
-        // Initialization here
-        
+        Raycasts = new Dictionary<string, RayCast2D>
+        {
+            {"right", GetNode<RayCast2D>("RayCastRight")},
+            {"left", GetNode<RayCast2D>("RayCastLeft")},
+            {"up", GetNode<RayCast2D>("RayCastUp")},
+            {"down", GetNode<RayCast2D>("RayCastDown")}
+        };
+
     }
 
+    public bool Move(string dir)
+    {
+        AnimationPlayer _player;
+        _player = GetNode<AnimationPlayer>("AnimationPlayer");
+        _player.PlaybackSpeed = Speed;
+        Facing = dir;
+        if (Raycasts[Facing].IsColliding())
+        {
+            return false;
+        }
+        _player.Play(Facing);
+        _moveTween = GetNode<Tween>("MoveTween");
+        _moveTween.InterpolateProperty(this, "position", Position, Position + _moves[Facing] * _tileSize, 
+            Convert.ToSingle(1.0 / Speed), Tween.TransitionType.Sine, Tween.EaseType.InOut);
+        _moveTween.Start();
+        return true;
+    }
+    
 //    public override void _Process(float delta)
 //    {
 //        // Called every frame. Delta is time since last frame.
 //        // Update game logic here.
 //        
 //    }
+    
+    private void _on_MoveTween_tween_completed(Godot.Object @object, NodePath key)
+    {
+        CanMove = true;
+    }
 }
+
+
+
